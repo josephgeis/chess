de1bb945d9cd98086d50b0736a1d1c7a805064bd
Joseph Geis <joseph@josephgeis.dev>
2026-01-13 22:56:41 -0700

    Implement moves for king, queen, knight, pawn

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -53,11 +53,59 @@ public class ChessPiece {
      * @return Collection of valid moves
      */
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
+        // This will ultimately be returned
         var moves = new Vector<ChessMove>();
+
+        // get current row and column
+        final var myRow = myPosition.getRow();
+        final var myCol = myPosition.getColumn();
+
+        //
+        int[] rowDeltas, colDeltas;
 
         switch(this.type) {
             case KING:
+            case QUEEN:
+                // Kings can move 1 square in any direction.
+                // Borrowing this as a template for queens.
+                rowDeltas = new int[]{1, 1, 0, -1, -1, -1, 0, 1};
+                colDeltas = new int[]{0, 1, 1,  1,  0, -1, -1, -1};
 
+                // Stop here for kings (only need one in each direction).
+                if (this.type == PieceType.KING) break;
+
+                // Queens can go up to 7 in any direction. Use the row/colDeltas made for the king as a basis,
+                // then multiply them by up to 7.
+                var kingRowDeltas = rowDeltas;
+                var kingColDeltas = colDeltas;
+                rowDeltas = new int[kingRowDeltas.length * 7];
+                colDeltas = new int[kingColDeltas.length * 7];
+
+                for (int multiplier = 1; multiplier < 8; multiplier++) {
+                    for (int i = 0; i < kingRowDeltas.length; i++) {
+                        rowDeltas[i + kingRowDeltas.length * (multiplier - 1)] = kingRowDeltas[i] * multiplier;
+                        colDeltas[i + kingColDeltas.length * (multiplier - 1)] = kingColDeltas[i] * multiplier;
+                    }
+                }
+                break;
+            case KNIGHT:
+                rowDeltas = new int[]{2, 1, -1, -2, -2, -1, 1, 2};
+                colDeltas = new int[]{1, 2, 2, 1, -1, -2, -2, -1};
+                break;
+            case PAWN:
+                rowDeltas = new int[]{1, 2};
+                colDeltas = new int[]{0, 0};
+                break;
+            default:
+                throw new RuntimeException("Not implemented");
+        }
+
+        ChessPosition newPosition;
+        for (int i = 0; i < rowDeltas.length; i++) {
+            newPosition = new ChessPosition(myRow + rowDeltas[i], myCol + colDeltas[i]);
+            if (newPosition.inBounds()) {
+                moves.add(new ChessMove(myPosition, newPosition, null));
+            }
         }
 
         return moves;
