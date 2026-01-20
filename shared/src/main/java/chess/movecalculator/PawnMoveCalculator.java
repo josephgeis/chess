1a50663f471bf59658399f7bb8db3f2daba3e00b
Joseph Geis <joseph@josephgeis.dev>
2026-01-20 15:55:50 -0700

    Implement all moves for pawns

--- shared/src/main/java/chess/movecalculator/PawnMoveCalculator.java
+++ shared/src/main/java/chess/movecalculator/PawnMoveCalculator.java
@@ -2,16 +2,38 @@ package chess.movecalculator;
 
 import chess.*;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 import java.util.Vector;
 
+import static chess.ChessPiece.PieceType.*;
+
 public class PawnMoveCalculator extends MoveCalculator {
+
+    static Collection<ChessMove> doPromotions(ChessMove chessMove) {
+        final var pieces = new ChessPiece.PieceType[] {
+                ChessPiece.PieceType.QUEEN,
+                ChessPiece.PieceType.ROOK,
+                ChessPiece.PieceType.BISHOP,
+                ChessPiece.PieceType.KNIGHT
+        };
+
+        var promotedMoves = new Vector<ChessMove>(4);
+
+        for (ChessPiece.PieceType piece : pieces) {
+            promotedMoves.add(new ChessMove(chessMove.getStartPosition(), chessMove.getEndPosition(), piece));
+        }
+
+        return promotedMoves;
+    }
+
     @Override
     public Collection<ChessMove> calculateMoves(ChessPiece piece, ChessBoard board, ChessPosition startPosition) {
         var moves = new Vector<ChessMove>();
 
         var rowFactor = piece.getTeamColor() == ChessGame.TeamColor.WHITE ? 1 : -1;
-        final var firstRow = piece.getTeamColor() == ChessGame.TeamColor.WHITE ? 2 : 8;
+        final var firstRow = piece.getTeamColor() == ChessGame.TeamColor.WHITE ? 2 : 7;
         final var lastRow = piece.getTeamColor() == ChessGame.TeamColor.WHITE ? 8 : 1;
 
         MoveVector singleAdvanceVector = new MoveVector(rowFactor, 0);
@@ -24,20 +46,29 @@ public class PawnMoveCalculator extends MoveCalculator {
         ChessPosition captureLeftPosition = captureLeftVector.applyTo(startPosition);
         ChessPosition captureRightPosition = captureRightVector.applyTo(startPosition);
 
-        if (singleAdvancePosition.isInBounds() && board.getPiece(singleAdvancePosition) == null)
-//            if (singleAdvancePosition.getRow() == lastRow) {
-                // Promotion
-//            } else {
-                moves.add(new ChessMove(startPosition, singleAdvancePosition, null));
-//            }
+        ChessMove newMove;
 
-        if (startPosition.getRow() == firstRow && doubleAdvancePosition.isInBounds() && board.getPiece(doubleAdvancePosition) == null)
-            moves.add(new ChessMove(startPosition, doubleAdvancePosition, null));
+        if (singleAdvancePosition.isInBounds() && board.getPiece(singleAdvancePosition) == null) {
+            newMove = new ChessMove(startPosition, singleAdvancePosition, null);
+            if (singleAdvancePosition.getRow() == lastRow) {
+                moves.addAll(doPromotions(newMove));
+            } else {
+                moves.add(newMove);
+
+                if (startPosition.getRow() == firstRow && doubleAdvancePosition.isInBounds() && board.getPiece(doubleAdvancePosition) == null)
+                    moves.add(new ChessMove(startPosition, doubleAdvancePosition, null));
+            }
+        }
 
         if (captureLeftPosition.isInBounds()) {
             ChessPiece captureLeftPiece = board.getPiece(captureLeftPosition);
             if (captureLeftPiece != null && captureLeftPiece.getTeamColor() != piece.getTeamColor()) {
-                moves.add(new ChessMove(startPosition, captureLeftPosition, null));
+                newMove = new ChessMove(startPosition, captureLeftPosition, null);
+                if (captureLeftPosition.getRow() == lastRow) {
+                    moves.addAll(doPromotions(newMove));
+                } else {
+                    moves.add(newMove);
+                }
             }
             // run promotions
         }
@@ -45,7 +76,12 @@ public class PawnMoveCalculator extends MoveCalculator {
         if (captureRightPosition.isInBounds()) {
             ChessPiece captureRightPiece = board.getPiece(captureRightPosition);
             if (captureRightPiece != null && captureRightPiece.getTeamColor() != piece.getTeamColor()) {
-                moves.add(new ChessMove(startPosition, captureRightPosition, null));
+                newMove = new ChessMove(startPosition, captureRightPosition, null);
+                if (captureRightPosition.getRow() == lastRow) {
+                    moves.addAll(doPromotions(newMove));
+                } else {
+                    moves.add(newMove);
+                }
             }
             // run promotions
         }
