13323c41180a6c48f85b73ef75d6a1cfc15a572e
Joseph Geis <joseph@josephgeis.dev>
2026-03-09 01:31:28 -0600

    Create MySQLGameDAO

---
+++ server/src/main/java/dataaccess/MySQLGameDAO.java
@@ -0,0 +1,40 @@
+package dataaccess;
+
+import model.GameData;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.Collection;
+
+public class MySQLGameDAO implements GameDAO {
+    @Override
+    public int createGame(GameData g) throws DataAccessException {
+        throw new DataAccessException("Not implemented");
+    }
+
+    @Override
+    public GameData retrieveGame(int gameID) throws DataAccessException {
+        throw new DataAccessException("Not implemented");
+    }
+
+    @Override
+    public void updateGame(int gameID, GameData updatedGame) throws DataAccessException {
+        throw new DataAccessException("Not implemented");
+    }
+
+    @Override
+    public Collection<GameData> retrieveAllGames() throws DataAccessException {
+        throw new DataAccessException("Not implemented");
+    }
+
+    @Override
+    public void clear() throws DataAccessException {
+        try (Connection conn = DatabaseManager.getConnection();
+             PreparedStatement stmt = conn.prepareStatement("delete from game where 1;")) {
+            stmt.executeUpdate();
+        } catch (SQLException e) {
+            throw new DataAccessException("Failed to clear table");
+        }
+    }
+}
