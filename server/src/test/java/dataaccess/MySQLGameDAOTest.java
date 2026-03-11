190ddd138b9b96df8801703f65943a4d3ba7f507
Joseph Geis <joseph@josephgeis.dev>
2026-03-11 15:36:39 -0600

    Write setUp method and clear test

--- server/src/test/java/dataaccess/MySQLGameDAOTest.java
+++ server/src/test/java/dataaccess/MySQLGameDAOTest.java
@@ -1,16 +1,35 @@
 package dataaccess;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assumptions.abort;
 
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class MySQLGameDAOTest {
 
     MySQLGameDAO gameDAO = new MySQLGameDAO();
 
+    static boolean clearTestRun = false;
+    static boolean clearTestPassed = false;
+
     @BeforeEach
     void setUp() {
+        if (!clearTestRun) {
+            return;
+        } else if (!clearTestPassed) {
+            abort("All other classes require the clear test to pass.");
+        }
+
+        try {
+            gameDAO.clear();
+        } catch (DataAccessException e) {
+            abort("Failed to clear table. Did the clear test pass?");
+        }
     }
 
     @Test
@@ -33,8 +52,20 @@ class MySQLGameDAOTest {
         fail();
     }
 
+    @Order(1)
     @Test
     void clear() {
-        fail();
+        clearTestRun = true;
+        assertDoesNotThrow(() -> gameDAO.clear());
+        try(var conn = DatabaseManager.getConnection();
+            var stmt = conn.prepareStatement("select count(*) from game;")) {
+            var rs = stmt.executeQuery();
+
+            rs.next();
+            assertEquals(0, rs.getInt(1));
+        } catch (SQLException | DataAccessException e) {
+            throw new RuntimeException(e);
+        }
+        clearTestPassed = true;
     }
 }
