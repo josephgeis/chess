22a3abdb84965493955888531ba60a02e22282a4
Joseph Geis <joseph@josephgeis.dev>
2026-03-14 17:08:55 -0600

    put specific functionality in subclass, more general in superclass

--- client/src/main/java/ui/Drawable.java
+++ client/src/main/java/ui/Drawable.java
@@ -12,11 +12,7 @@ public abstract class Drawable {
     }
 
     public void setTextGraphics(TextGraphics parentTextGraphics) {
-        TerminalSize terminalSize = parentTextGraphics.getSize();
-
-        this.textGraphics = parentTextGraphics.newTextGraphics(
-                TerminalPosition.TOP_LEFT_CORNER,
-                terminalSize.withRelativeRows(-1));
+        this.textGraphics = parentTextGraphics;
     }
 
     abstract public void draw();
