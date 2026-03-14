82199ecea0565fc256fdd5d76732776d83508de4
Joseph Geis <joseph@josephgeis.dev>
2026-03-14 02:48:01 -0600

    Make Drawable abstract classes

--- client/src/main/java/ui/Drawable.java
+++ client/src/main/java/ui/Drawable.java
@@ -1,9 +1,23 @@
 package ui;
 
+import com.googlecode.lanterna.TerminalPosition;
+import com.googlecode.lanterna.TerminalSize;
 import com.googlecode.lanterna.graphics.TextGraphics;
 
-public interface Drawable {
-    void setTextGraphics(TextGraphics parentTextGraphics);
+public abstract class Drawable {
+    protected TextGraphics textGraphics;
 
-    void draw();
+    public Drawable(TextGraphics parentTextGraphics) {
+        setTextGraphics(parentTextGraphics);
+    }
+
+    public void setTextGraphics(TextGraphics parentTextGraphics) {
+        TerminalSize terminalSize = parentTextGraphics.getSize();
+
+        this.textGraphics = parentTextGraphics.newTextGraphics(
+                TerminalPosition.TOP_LEFT_CORNER,
+                terminalSize.withRelativeRows(-1));
+    }
+
+    abstract public void draw();
 }
