4e3cd5fcc71c2a5c0e7b3af0f0a4c585c955ba64
Joseph Geis <joseph@josephgeis.dev>
2026-03-14 11:25:11 -0600

    Trim unused imports

--- client/src/main/java/ui/TerminalController.java
+++ client/src/main/java/ui/TerminalController.java
@@ -1,8 +1,5 @@
 package ui;
 
-import client.ClientState;
-import com.googlecode.lanterna.TerminalPosition;
-import com.googlecode.lanterna.TextColor;
 import com.googlecode.lanterna.graphics.TextGraphics;
 import com.googlecode.lanterna.input.KeyStroke;
 import com.googlecode.lanterna.input.KeyType;
