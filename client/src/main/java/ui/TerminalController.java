197a7b1d422ea3243bdc3303f75144a58f5ab5ea
Joseph Geis <joseph@josephgeis.dev>
2026-03-14 11:18:19 -0600

    Add event handler architecture

--- client/src/main/java/ui/TerminalController.java
+++ client/src/main/java/ui/TerminalController.java
@@ -13,6 +13,11 @@ import ui.views.ChessBoardView;
 import ui.views.View;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
+
+import static com.googlecode.lanterna.input.KeyType.*;
 
 /**
  * Responsible for drawing the parts of the screen and receives keyboard input.
@@ -27,6 +32,40 @@ public class TerminalController {
 
     View view;
     MenuBar menuBar;
+
+    HashMap<EventType, HashSet<Runnable>> eventHandlers = new HashMap<>();
+
+    public enum EventType {
+        LOG_IN,
+        REGISTER,
+        QUIT_PROGRAM,
+        NEW_GAME,
+        JOIN_GAME,
+        LIST_GAME,
+        SPECTATE_GAME,
+        LOG_OUT
+    }
+
+    void fireEvent(EventType eventType) {
+        HashSet<Runnable> callbacks = eventHandlers.get(eventType);
+        if (callbacks == null) {
+            return;
+        }
+
+        for (Runnable callback : callbacks) {
+            callback.run();
+        }
+    }
+
+    public void registerEventHandler(EventType eventType, Runnable callback) {
+        HashSet<Runnable> callbacks = eventHandlers.computeIfAbsent(eventType, k -> new HashSet<>());
+        callbacks.add(callback);
+    }
+
+    public void removeEventHandler(EventType eventType, Runnable callback) {
+        HashSet<Runnable> callbacks = eventHandlers.computeIfAbsent(eventType, k -> new HashSet<>());
+        callbacks.remove(callback);
+    }
 
     public void eventLoop() throws IOException {
         if (screen.doResizeIfNecessary() != null) {
@@ -46,8 +85,17 @@ public class TerminalController {
     }
 
     protected void handleInput(KeyStroke keyStroke) {
-        if (!ClientState.isLoggedIn() && keyStroke.getKeyType() == KeyType.F6) {
-            ClientState.quitProgram();
+        Set<KeyType> fnKeys = Set.of(new KeyType[]{F1, F2, F3, F4, F5, F6});
+
+        KeyType keyType = keyStroke.getKeyType();
+        EventType eventType = null;
+
+        if (fnKeys.contains(keyType)) {
+            eventType = menuBar.getEventForMenuKey(keyType);
+        }
+
+        if (eventType != null) {
+            fireEvent(eventType);
         }
     }
 
--- client/src/main/java/ui/menubar/MenuItems.java
+++ client/src/main/java/ui/menubar/MenuItems.java
@@ -1,14 +1,16 @@
 package ui.menubar;
 
+import static ui.TerminalController.EventType.*;
+
 public abstract class MenuItems {
     public static MenuItems NOT_LOGGED_IN = new MenuItems() {
         @Override
         protected MenuBarItem itemAt(int i) {
             return switch (i) {
-                case 1 -> new MenuBarItem("Login");
-                case 2 -> new MenuBarItem("Register");
-                case 5 -> new MenuBarItem("Help");
-                case 6 -> new MenuBarItem("Quit");
+                case 1 -> new MenuBarItem("Login", LOG_IN);
+                case 2 -> new MenuBarItem("Register", REGISTER);
+                case 5 -> new MenuBarItem("Help", null);
+                case 6 -> new MenuBarItem("Quit", QUIT_PROGRAM);
                 default -> null;
             };
         }
@@ -18,12 +20,12 @@ public abstract class MenuItems {
         @Override
         protected MenuBarItem itemAt(int i) {
             return switch(i) {
-                case 1 -> new MenuBarItem("New Game");
-                case 2 -> new MenuBarItem("JoinGame");
-                case 3 -> new MenuBarItem("ListGame");
-                case 4 -> new MenuBarItem("SpecGame");
-                case 5 -> new MenuBarItem("Help");
-                case 6 -> new MenuBarItem("Log Out");
+                case 1 -> new MenuBarItem("New Game", NEW_GAME);
+                case 2 -> new MenuBarItem("JoinGame", JOIN_GAME);
+                case 3 -> new MenuBarItem("ListGame", LIST_GAME);
+                case 4 -> new MenuBarItem("SpecGame", SPECTATE_GAME);
+                case 5 -> new MenuBarItem("Help", null);
+                case 6 -> new MenuBarItem("Log Out", LOG_OUT);
                 default -> null;
             };
         }
