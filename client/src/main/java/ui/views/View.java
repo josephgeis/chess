d048bed22098689247beb4409d459d27983d45c8
Joseph Geis <joseph@josephgeis.dev>
2026-03-14 17:08:55 -0600

    put specific functionality in subclass, more general in superclass

--- client/src/main/java/ui/views/View.java
+++ client/src/main/java/ui/views/View.java
@@ -1,5 +1,7 @@
 package ui.views;
 
+import com.googlecode.lanterna.TerminalPosition;
+import com.googlecode.lanterna.TerminalSize;
 import com.googlecode.lanterna.graphics.TextGraphics;
 import ui.Drawable;
 
@@ -9,6 +11,14 @@ public abstract class View extends Drawable {
         super(parentTextGraphics);
     }
 
+    @Override
+    public void setTextGraphics(TextGraphics parentTextGraphics) {
+        TerminalSize terminalSize = parentTextGraphics.getSize();
+
+        super.setTextGraphics(parentTextGraphics.newTextGraphics(
+                TerminalPosition.TOP_LEFT_CORNER,
+                terminalSize.withRelativeRows(-1)));
+    }
 
     @Override
     abstract public void draw();
