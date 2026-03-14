a38e12ddcce994ffbfb2bf11afeb0cd9df9cf040
Joseph Geis <joseph@josephgeis.dev>
2026-03-14 00:06:07 -0600

    Clean up unused code

--- client/src/main/java/ui/menubar/MenuItems.java
+++ client/src/main/java/ui/menubar/MenuItems.java
@@ -29,14 +29,6 @@ public abstract class MenuItems {
         }
     };
 
-    final public MenuBarItem getItem(int i) {
-        if (i < 1 || i > 10) {
-            throw new IllegalStateException("Unexpected value: " + i);
-        }
-
-        return itemAt(i);
-    }
-
     protected MenuBarItem itemAt(int i) {
         return null;
     }
