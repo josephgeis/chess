4c79eb4ea6d43ac2378862068c200db896d41ff6
Joseph Geis <joseph@josephgeis.dev>
2026-03-15 16:34:37 -0600

    Add form for Login modal

--- client/src/main/java/ui/modals/LoginModal.java
+++ client/src/main/java/ui/modals/LoginModal.java
@@ -4,10 +4,13 @@ import com.googlecode.lanterna.TerminalPosition;
 import com.googlecode.lanterna.TerminalSize;
 import com.googlecode.lanterna.TextColor;
 import com.googlecode.lanterna.graphics.TextGraphics;
+import com.googlecode.lanterna.input.KeyStroke;
 import ui.TerminalController;
 
-public abstract class LoginModal extends Modal {
+  public abstract class LoginModal extends Modal {
     int field = 0;
+
+    String[] fields = {"", "", null, null};
 
     public LoginModal(TextGraphics parentTextGraphics, TerminalController terminalController) {
         super(parentTextGraphics, terminalController);
@@ -15,7 +18,32 @@ public abstract class LoginModal extends Modal {
 
     @Override
     protected TerminalSize getSize() {
-        return new TerminalSize(40, 8);
+        return new TerminalSize(37, 9);
+    }
+
+    @Override
+    public void onKeyStroke(KeyStroke keyStroke) {
+        super.onKeyStroke(keyStroke);
+        switch (keyStroke.getKeyType()) {
+            case Character -> {
+                if (fields[field] != null) {
+                    fields[field] = (fields[field] + keyStroke.getCharacter())
+                            .substring(0, Integer.min(fields[field].length() + 1, 32));
+                }
+            }
+            case Backspace -> {
+                if (fields[field] != null) {
+                    fields[field] = fields[field].substring(0, Integer.max(fields[field].length() - 1, 0));
+                }
+            }
+            case ArrowDown, ArrowRight, Tab -> field = (field + 1) % fields.length;
+            case ArrowUp, ArrowLeft, ReverseTab -> field = (field + fields.length - 1) % fields.length;
+            case Enter -> {
+                if (field == 3) {
+                    close();
+                }
+            }
+        }
     }
 
     @Override
@@ -23,17 +51,40 @@ public abstract class LoginModal extends Modal {
         super.draw();
         textGraphics.putString(2, 0, "Login");
 
-        textGraphics.putString(TerminalPosition.OFFSET_1x1, "Username:");
+        TerminalPosition fieldStartPosition = TerminalPosition.OFFSET_1x1.withRelativeColumn(1);
+        defaultColor();
+        textGraphics.putString(fieldStartPosition, "Username:");
+        highlightSelectedField(0);
+        textGraphics.putString(fieldStartPosition.withRelativeRow(1), "[" );
+        textGraphics.putString(fieldStartPosition.withRelative(1, 1), "%-32s".formatted(fields[0].replace(' ', '␣')));
+        textGraphics.putString(fieldStartPosition.withRelative(32, 1), "]" );
 
-        if (field == 0) {
-            textGraphics.setBackgroundColor(TextColor.ANSI.YELLOW);
-            textGraphics.setForegroundColor(TextColor.ANSI.WHITE_BRIGHT);
-        } else {
-            textGraphics.setBackgroundColor(TextColor.ANSI.WHITE);
-        }
+        fieldStartPosition = fieldStartPosition.withRelativeRow(3);
+        defaultColor();
+        textGraphics.putString(fieldStartPosition, "Password:");
+        highlightSelectedField(1);
+        textGraphics.putString(fieldStartPosition.withRelativeRow(1), "[" );
+        textGraphics.putString(fieldStartPosition.withRelative(1, 1), "%-32s".formatted("*".repeat(fields[1].length())));
+        textGraphics.putString(fieldStartPosition.withRelative(32, 1), "]" );
 
-        textGraphics.putString(TerminalPosition.OFFSET_1x1.withRelative(0, 1), "[" );
-        textGraphics.fillRectangle(TerminalPosition.OFFSET_1x1.withRelative(1, 1), new TerminalSize(32, 1), '_');
-        textGraphics.putString(TerminalPosition.OFFSET_1x1.withRelative(32, 1), "]" );
+        fieldStartPosition = fieldStartPosition.withRelativeRow(3);
+        defaultColor();
+        highlightSelectedField(2);
+        textGraphics.putString(fieldStartPosition, "<Submit>");
+
+        fieldStartPosition = fieldStartPosition.withRelativeColumn(9);
+        defaultColor();
+        highlightSelectedField(3);
+        textGraphics.putString(fieldStartPosition, "<Cancel>");
     }
-}
+
+        private void highlightSelectedField(int field) {
+          if (this.field == field) {
+              textGraphics.setBackgroundColor(TextColor.ANSI.YELLOW);
+              textGraphics.setForegroundColor(TextColor.ANSI.WHITE_BRIGHT);
+          } else {
+              textGraphics.setBackgroundColor(TextColor.ANSI.WHITE);
+              textGraphics.setForegroundColor(TextColor.ANSI.BLACK);
+          }
+        }
+  }
