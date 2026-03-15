ed7fbd9347e97de8e325f9507fa8caa33bf69d67
Joseph Geis <joseph@josephgeis.dev>
2026-03-14 22:57:21 -0600

    Refactor event handling system

--- client/src/main/java/ui/menubar/MenuBarItem.java
+++ client/src/main/java/ui/menubar/MenuBarItem.java
@@ -1,8 +1,8 @@
 package ui.menubar;
 
-import ui.TerminalController;
+import ui.EventPublisher;
 
-public record MenuBarItem(String name, TerminalController.EventType eventType) {
+public record MenuBarItem(String name, EventPublisher.EventType eventType) {
     public String menuTitle() {
         return name.substring(0, Integer.min(8, name.length()));
     }
