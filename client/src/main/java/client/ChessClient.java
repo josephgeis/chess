b8c32beba66f6e8596625ff3446b766a68713629
Joseph Geis <josephegeis@gmail.com>
2026-03-21 20:09:04 -0600

    Abstract error preprocessing

--- client/src/main/java/client/ChessClient.java
+++ client/src/main/java/client/ChessClient.java
@@ -5,6 +5,7 @@ import request.LoginRequest;
 import response.LoginResponse;
 
 import java.util.concurrent.CompletableFuture;
+import java.util.function.Supplier;
 
 public class ChessClient {
     private final ServerFacade serverFacade;
@@ -19,40 +20,33 @@ public class ChessClient {
         return clientState;
     }
 
+    CompletableFuture<Void> makeRequest(Supplier<CompletableFuture<Void>> request) {
+        return makeRequest(request, Void.TYPE);
+    }
+
+    <T> CompletableFuture<T> makeRequest(Supplier<CompletableFuture<T>> request, Class<T> ignored) {
+        return request.get().exceptionallyCompose(throwable -> {
+            if (throwable instanceof ServerFacade.ErrorResponseException) {
+                return CompletableFuture.failedFuture(throwable);
+            } else {
+                return CompletableFuture.failedFuture(new Throwable("Unexpected error"));
+            }
+        });
+    }
+
     public CompletableFuture<LoginResponse> makeLoginRequest(String username, String password) {
         LoginRequest request = new LoginRequest(username, password);
-        try {
-            return serverFacade.loginUserAsync(request).thenApply(loginResponse -> {
-                        clientState.setAuthData(
-                                new AuthData(loginResponse.authToken(),
-                                        loginResponse.username())
-                        );
-                        return loginResponse;
-                    })
-                    .exceptionallyCompose(throwable -> {
-                        if (throwable.getCause() instanceof ServerFacade.ErrorResponseException) {
-                            return CompletableFuture.failedFuture(throwable.getCause());
-                        } else {
-                            return CompletableFuture.failedFuture(new Throwable("Unexpected error"));
-                        }
-                    });
-        } catch (ServerFacade.ServerFacadeException e) {
-            return CompletableFuture.failedFuture(e.getCause());
-        }
+        return makeRequest(() -> serverFacade.loginUserAsync(request), LoginResponse.class)
+                .thenApply(loginResponse -> {
+                    clientState.setAuthData(
+                            new AuthData(loginResponse.authToken(),
+                                    loginResponse.username())
+                    );
+                    return loginResponse;
+                });
     }
 
     public CompletableFuture<Void> makeLogoutRequest() {
-        try {
-            return serverFacade.logoutUserAsync(clientState.getAuthToken())
-                    .exceptionallyCompose(throwable -> {
-                        if (throwable instanceof ServerFacade.ErrorResponseException) {
-                            return CompletableFuture.failedFuture(throwable);
-                        } else {
-                            return CompletableFuture.failedFuture(new Throwable("Unexpected error"));
-                        }
-                    });
-        } catch (ServerFacade.ServerFacadeException e) {
-            return CompletableFuture.failedFuture(e.getCause());
-        }
+        return makeRequest(() -> serverFacade.logoutUserAsync(clientState.getAuthToken()));
     }
 }
